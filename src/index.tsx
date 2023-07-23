c4c4fa9e3bb8425dc1099c1c83d02e6b30fa946c
andreas-schoch <andreas_schoch@outlook.com>
2023-07-23 20:29:39 +0200

    import webrtc-adapter to make it part of the build

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 /* @refresh reload */
 import {render} from 'solid-js/web';
-
-import './index.css';
+import adapter from 'webrtc-adapter';
 import {App} from './App';
+import './index.css';
 
+console.log('Quackmole browser used:' + adapter.browserDetails.browser);
 const root = document.getElementById('root');
 if (!root) throw new Error('Root element not found');
 render(() => <App/>, root);
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
-import { VitePWA } from 'vite-plugin-pwa';
+// import { VitePWA } from 'vite-plugin-pwa'; // causes annoying caching behaviour with default options which isn't desirable at this development stage
 import mkcert from'vite-plugin-mkcert';
 
 
 
 export default defineConfig({
-  plugins: [solidPlugin(), VitePWA(), mkcert()],
+  optimizeDeps: {
+    exclude: ['webrtc-adapter']
+  },
+  plugins: [solidPlugin(), /* VitePWA(),*/ mkcert()],
   server: {
     port: 3000,
   },
