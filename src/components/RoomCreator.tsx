3920377062dcaf734043dbeb9590b7878e5f1967
andreas-schoch <andreas_schoch@outlook.com>
2023-07-24 16:29:07 +0200

    add QR code to room creator

--- src/components/RoomCreator.tsx
+++ src/components/RoomCreator.tsx
@@ -1,21 +1,39 @@
 import type { Component } from 'solid-js';
-import { createMemo, createSignal, Show } from 'solid-js';
+import { createEffect, createMemo, createSignal, Show } from 'solid-js';
 import { A } from '@solidjs/router';
 import { TextCopyOutput } from './TextCopyOutput';
 import { IAdminRoom } from 'quackamole-shared-types';
 import { QuackamoleHttpClient } from 'quackamole-rtc-client';
+import QRCodeStyling from 'qr-code-styling';
 
 export const RoomCreator: Component = () => {
   const [room, setRoom] = createSignal<IAdminRoom | null>(null);
   const [error, setError] = createSignal('');
 
-  const roomGuestUrl = createMemo(() => `${location.origin}/${room()?.id}`);
-  const roomAdminUrl = createMemo(() => `${location.origin}/${room()?.adminId}`);
+  const roomGuestUrl = createMemo(() => `${location.origin}/#/${room()?.id}`);
+  const roomAdminUrl = createMemo(() => `${location.origin}/#/${room()?.adminId}`);
+
+  let wrapperQRRef: HTMLElement | null = null;
 
   async function createRoom() {
     const room = await QuackamoleHttpClient.createRoom();
     room instanceof Error ? setError(room.message) : setRoom(room);
   }
+
+  createEffect(() => {
+    const r = room();
+    if (!r) return;
+    new QRCodeStyling({
+      width: 180,
+      height: 180,
+      type: 'canvas',
+      data: roomAdminUrl(),
+      image: undefined,
+      dotsOptions: {color: '#4267b2', type: 'rounded'},
+      backgroundOptions: {color: '#e9ebee'},
+      imageOptions: {crossOrigin: 'anonymous', margin: 10}
+    }).append(wrapperQRRef!);
+  });
 
   return <>
     <div class="flex justify-center items-center flex-col h-full bg-stone-950">
@@ -31,9 +49,9 @@ export const RoomCreator: Component = () => {
           <div class="text-2xl m-auto mb-6">Created</div>
           <div>Share this link with Guests:</div>
           <TextCopyOutput text={roomGuestUrl} />
-
           <div>Share this link with Admins:</div>
           <TextCopyOutput text={roomAdminUrl} />
+          <div class="flex justify-center" ref={el => wrapperQRRef = el}  />
           <A href={`/${room()?.adminId}`} class="mt-3 flex w-full justify-center rounded-md bg-indigo-600 px-6 py-3 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Join as Moderator</A>
         </Show>
       </div>
